c367f4ec0d29f055b7bf52cdcc0e321e4adb09e3
Rajakuruvpm <rajakuruvpm11@gmail>
2026-01-28 13:45:35 +0530

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base:'/demo1/',
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/demo1/',
+  base: "/demo1/"
 })
